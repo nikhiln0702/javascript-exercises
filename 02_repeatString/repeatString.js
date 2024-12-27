fa66d1971d7cd0c6f166116200cb75fae7a4e2a9
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2024-12-27 14:22:37 +0000

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string,count) {
+    let word="";
+    let flag=0
+    if(count<0){
+        flag=1;
+    }
+    for(let i=0;i<count;i++){
+        word=word+string;
+    }
+    if(flag==0){
+    return word;}
+    else{
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
