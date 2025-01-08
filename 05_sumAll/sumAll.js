f99199ebd4a4af30be2197623c78c54e93594b91
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2025-01-08 18:20:58 +0000

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(lower_num,higher_num) {
+    let sum=0;
+    let negativeflag=0;
+    if(lower_num>higher_num){
+        let temp=lower_num;
+        lower_num=higher_num;
+        higher_num=temp;
+    }
+    if(lower_num<0||higher_num<0||Number.isInteger(lower_num)==false||Number.isInteger(higher_num)==false||typeof(lower_num)!="number"||typeof(higher_num)!="number"){
+        negativeflag=1;
+    }
+    if(negativeflag==0){
+    for(let i=lower_num;i<=higher_num;i++){
+        sum=sum+i;
+    }
+    return sum;}
+    else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
