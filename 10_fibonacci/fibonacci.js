9bcb99f3ef6effeca13d4a2b156d96fa5d2ba57a
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2025-01-27 17:59:38 +0000

    completed 09_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num=parseInt(num);
+    if(num>=0){
+    if(num==0){
+        return 0;
+    }
+    else if(num==1){
+        return 1;
+    }
+    else{
+        let f=0;
+        let s=1;
+        let t=0;
+        for(let i=2;i<=num;i++){
+            t=f+s;
+            f=s;
+            s=t;
+        }
+        return t;
+    }}
+    else{
+        return "OOPS";
+    }
 
 };
 
