2121e2ace96eb30c83100b8fc05771d6630f0c4d
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2025-01-27 17:45:13 +0000

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    for(let i=0;i<((string.length/2)|0);i++){
+        // console.log(string[i]);
+        // console.log(string[string.length-i-1]);
+        if(string[i]!=string[string.length-i-1]){
+            return false;
+            break;
+        }
+    }
+    return true;
 
 };
 
