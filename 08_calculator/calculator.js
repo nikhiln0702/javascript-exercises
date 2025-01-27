d7b6cc3bed7fc833135d8e49e3e5e36df954738e
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2025-01-27 17:25:09 +0000

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum=array.reduce((total,currentItem)=>{
+    return total+currentItem
+  },0)
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product=array.reduce((total,currentItem)=>{
+    return total*currentItem
+  },1)
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fact=1;
+	for(let i=1;i<=num;i++){
+    fact=fact*i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
